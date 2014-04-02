553ee97888cd4384f36585d12985e18d000144fe
RamblingMushroom <Graeme.B.Peters@gmail.com>
2014-04-01 23:18:19 -0500

    Prefabs and an island

--- UnityProject/Assets/Standard Assets/Character Controllers/Sources/Scripts/SkyboxScript.cs
+++ UnityProject/Assets/Standard Assets/Character Controllers/Sources/Scripts/SkyboxScript.cs
@@ -5,6 +5,7 @@ public class SkyboxScript : MonoBehaviour {
 	
 	float timeOfDay;
 	string skyboxLoc = "Standard Assets/Skyboxes/";
+	public float timeSpeed = 20f;
 	public Material daySky;
 	public Material nightSky;
 	public Material dawnSky;
@@ -24,12 +25,12 @@ public class SkyboxScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-		timeOfDay += Time.deltaTime * 20;
+		float timePassage = Time.deltaTime * timeSpeed;
+		timeOfDay += timePassage;
 		timeOfDay %= 360;
 		float t = (timeOfDay % 90) / 90;
 		t = timeOfDay / 360;
-
+		transform.Rotate (timePassage, 0, 0, Space.Self);
 
 		if (timeOfDay < 90f && currSky != dawnSky) {
 			lerpSky = dawnSky;
@@ -52,7 +53,7 @@ public class SkyboxScript : MonoBehaviour {
 		lerpSky.Lerp (currSky, nextSky, Time.deltaTime * 20 / 90);
 		//Oh wait this only lerps through colours so fuck it
 
-		RenderSettings.skybox = lerpSky;
+		RenderSettings.skybox.SetTexture("_FrontTex", currSky.GetTexture("_FrontTex"));
 		//(RenderSettings.skybox).Lerp(currSky, nextSky, t);
 	}
 }
