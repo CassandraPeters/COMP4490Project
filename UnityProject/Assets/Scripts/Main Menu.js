db6a4d03fb6d072326c0ec891080bc0bfceb7c93
Lyndon-q <umquigll@myumanitoba.ca>
2014-04-01 22:52:09 -0500

    Added Menus and Some cool features. Added a desert.

---
+++ UnityProject/Assets/Scripts/Main Menu.js
@@ -0,0 +1,35 @@
+﻿var isQuit=false;
+var isSelect=false;
+var isRandom=false;
+var isOption=false;
+
+function OnMouseEnter(){
+	//change text color
+	renderer.material.color=Color.red;
+}
+
+function OnMouseExit(){
+	//change text color
+	renderer.material.color=Color.white;
+}
+
+function OnMouseUp(){
+	//is this quit
+	if (isSelect==true){
+		Application.LoadLevel(2);
+	}
+	if (isOption==true){
+		Application.LoadLevel(1);
+	}
+	if (isQuit==true) {
+		//quit the game
+		Application.Quit();
+	}
+}
+
+function Update(){
+	//quit game if escape key is pressed
+	if (Input.GetKey(KeyCode.Escape)) { 
+		Application.Quit();
+	}
+}
---
+++ UnityProject/Assets/Scripts/Select Menu.js
@@ -0,0 +1,51 @@
+﻿var isQuit=false;
+var isIsland1=false;
+var isIsland2=false;
+var isDesert=false;
+var myTexture;
+var myPlane;
+
+function OnMouseEnter(){
+	myPlane = GameObject.Find("Plane");
+	//change text color
+	renderer.material.color=Color.red;
+	if (isIsland1==true){
+		myTexture = Resources.LoadAssetAtPath("Assets/Graphics/Textures/MainMenuTexture.png",typeof(Texture));
+		myPlane.renderer.material.mainTexture = myTexture;
+	}
+	if (isDesert == true){
+		myTexture = Resources.LoadAssetAtPath("Assets/Graphics/Textures/DesertImage.png",typeof(Texture));
+		myPlane.renderer.material.mainTexture = myTexture;
+	}
+}
+
+function OnMouseExit(){
+	//change text color
+	renderer.material.color=Color.white;
+}
+
+function OnMouseUp(){
+	if (isIsland1==true){
+		//Load Island 1
+		Application.LoadLevel(3);
+	}
+	if (isIsland2==true){
+		//Load Island 2
+		
+	}
+	if (isDesert==true){
+		//Load Desert Level
+		Application.LoadLevel(4);
+	}
+	if (isQuit==true) {
+		//quit the game
+		Application.Quit();
+	}
+}
+
+function Update(){
+	//quit game if escape key is pressed
+	if (Input.GetKey(KeyCode.Escape)) { 
+		Application.Quit();
+	}
+}
