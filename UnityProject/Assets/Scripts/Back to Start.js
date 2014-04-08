9bea9822ef6af51377f9d36203ab6cef8a960106
Lyndon-q <umquigll@myumanitoba.ca>
2014-04-08 00:26:17 -0500

    Added Random Island to the Main Menu.

---
+++ UnityProject/Assets/Scripts/Back to Start.js
@@ -0,0 +1,6 @@
+﻿
+function Update () {
+	if (Input.GetKey(KeyCode.Escape)) { 
+		Application.LoadLevel(0);
+	}
+}
--- UnityProject/Assets/Scripts/Main Menu.js
+++ UnityProject/Assets/Scripts/Main Menu.js
@@ -3,6 +3,10 @@ var isSelect=false;
 var isRandom=false;
 var isOption=false;
 
+function Start(){
+	Screen.showCursor = true;
+}
+
 function OnMouseEnter(){
 	//change text color
 	renderer.material.color=Color.red;
@@ -20,6 +24,9 @@ function OnMouseUp(){
 	}
 	if (isOption==true){
 		Application.LoadLevel(1);
+	}
+	if (isRandom == true){
+		Application.LoadLevel(6);
 	}
 	if (isQuit==true) {
 		//quit the game
